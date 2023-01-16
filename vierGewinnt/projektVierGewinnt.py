27ab1849a4bcf50f8627390faacfb08af28e9c61
Nada Hassan <s51992@edu.campus02.at>
2023-01-16 14:30:00 +0100

    Klasse Spieler hineinprogrammiert.

--- vierGewinnt/projektVierGewinnt.py
+++ vierGewinnt/projektVierGewinnt.py
@@ -1,2 +1,10 @@
+class Spieler():
+	def __init__(self, Spielernummer:int, Farbe:str, Name:str):
+		self.Spielernummer = Spielernummer
+		self.Farbe = Farbe
+		self.Name = Name
+
+
+
 if __name__ == '__main__'
 	pass
