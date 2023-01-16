3ad43cdf7ec1f65f8cfbfa1a4ec6d6d6db995f35
Lea Schmoll <lea.schmoll@gmx.at>
2023-01-16 14:34:06 +0100

    Spielfeld Klasse angelegt

--- vierGewinnt/projektVierGewinnt.py
+++ vierGewinnt/projektVierGewinnt.py
@@ -1,2 +1,19 @@
-if __name__ == '__main__'
-	pass
+class Spielfeld:
+    def __init__(self, grid: list):
+        self.grid = grid
+
+    def __repr__(self):
+        return f'grid:{self.grid}'
+
+    def spielfeld_erstellen(self):  # TODO
+        pass
+
+    def spielfeld_aktualisieren(self):  # TODO
+        pass
+
+    def gewinn_abfragen(self):  # TODO
+        pass
+
+
+if __name__ == '__main__':
+    pass
