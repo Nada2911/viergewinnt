150d66d8b2723ff2fc551f11fd7d650c568e6f83
Lea Schmoll <lea.schmoll@gmx.at>
2023-02-02 22:04:17 +0100

    Computerspieler abgeleitete Klasse erstellt field initialisiert

--- vierGewinnt/projektVierGewinnt.py
+++ vierGewinnt/projektVierGewinnt.py
@@ -1,12 +1,17 @@
+from random import randint
+
+
 class Spielfeld:
-    def __init__(self, grid: list):
-        self.grid = grid
+    def __init__(self):
+        field = [['X' for i in range(7)] for j in range(6)]
+        self.__grid = field
+
+    @property
+    def grid(self):
+        return self.__grid
 
     def __repr__(self):
         return f'grid:{self.grid}'
-
-    def spielfeld_erstellen(self):  # TODO
-        pass
 
     def spielfeld_aktualisieren(self):  # TODO
         pass
@@ -14,14 +19,29 @@ class Spielfeld:
     def gewinn_abfragen(self):  # TODO
         pass
 
-class Spieler():
-	def __init__(self, Spielernummer:int, Farbe:str, Name:str):
-		self.Spielernummer = Spielernummer
-		self.Farbe = Farbe
-		self.Name = Name
 
+class Spieler():
+    def __init__(self, farbe: str, name: str):
+        self.farbe = farbe
+        self.name = name
+
+
+class Computer(Spieler):
+
+    def __init__(self, farbe: str, name: str):
+        super().__init__(farbe, name)
+
+    def entscheiden(self) -> int:
+        """
+        Computer gibt einen gültigen Zug ab
+        Der Zug ist eine Zufallszahl zwischen 0 und 6.
+
+        :return:
+            Wert für Zug: int
+        """
+        zug = randint(0, 6)
+        return zug
 
 
 if __name__ == '__main__':
-	pass
-
+    pass
